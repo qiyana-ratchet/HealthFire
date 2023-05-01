3428c79bf383103225e897718be1fc1e96c5cc82
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-05-01 23:28:41 +0900

    Add Ranking screen

---
+++ screens/RankingScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function RankingScreen() {  // 랭킹 화면
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This is the Ranking Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
